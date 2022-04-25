24e06c890c5e68c256f1a403611f70eac9c8715e
MarcoSantander7 <81274258+MarcoSantander7@users.noreply.github.com>
2022-04-25 18:23:52 -0500

    x

--- src/main/java/com/proyectointegrador/proyecto/Controllers/UsuarioRestController.java
+++ src/main/java/com/proyectointegrador/proyecto/Controllers/UsuarioRestController.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins= {"http://localhost:4200"})
+@CrossOrigin(origins= {"https://cmarcosantander7.github.io/EraseUnaVezFront"})
 @RestController
 @RequestMapping("/api/auth")
 public class UsuarioRestController {
